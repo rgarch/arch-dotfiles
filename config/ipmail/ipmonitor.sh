7561ecfa343f06629bda42d94e0d268f6bc76fc6
rgarch <rs87428@gmail.com>
2014-09-03 05:01:36 -0500

    update

--- config/ipmail/ipmonitor.sh
+++ config/ipmail/ipmonitor.sh
@@ -3,13 +3,13 @@
 #
 #Get the WAN IP
 exip=`lynx -dump http://www.networksecuritytoolkit.org/nst/tools/ip.php | head -n 2`
-echo $exip - `date` > /home/ricky/.scripts/ipmail/ipmonitor
+echo $exip - `date` > /home/x/.scripts/ipmail/ipmonitor
 #
 # Subject
 SUBJECT="System IP"
 # Email To....
-EMAIL="rs87428@gmail.com"
+EMAIL=""
 # Message
-EMAILMESSAGE="/home/ricky/.scripts/ipmail/ipmonitor"
+EMAILMESSAGE="/home/x/.scripts/ipmail/ipmonitor"
 # Send email
 mail -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
