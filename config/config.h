8f191e821290bf122a3870c88fa23d3e3283910d
rgarch <rs87428@gmail.com>
2015-09-28 20:54:07 +0900

    changed config

--- config/config.h
+++ config/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Source Code Pro:pixelsize=14:antialias=true:autohint=true";
+static char font[] = "Source Code Pro:pixelsize=15:antialias=false:autohint=false";
 static int borderpx = 2;
 
 /*
